047d1dceead1649643b6e1fb3315df6f88e1bdae
Лейкин Андрей Романович <Leykin.AR@kamaz.org>
2017-10-11 16:41:46 +0300

    last commit

--- dev/static/js/app.js
+++ dev/static/js/app.js
@@ -7,5 +7,52 @@
 		slidesToShow: 5,
 		autoplay: true,
 		autoplaySpeed: 2000,
-	})
+		responsive: [
+		{
+			breakpoint: 960,
+			settings: {
+				slidesToShow: 3
+				}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 2
+				}
+		}
+		]
+	});
+
+	var link = $('.menu__humburger'),
+		list = $('.menu__list'),
+		contacts = $('.header__contacts'),
+		activeLink = 'menu__humburger--active',
+		flag = true,
+		duration = 400;
+
+		link.click(function() {
+			if(flag) {
+				link.addClass(activeLink);
+				list.slideDown(duration);
+				setTimeout(function(){
+					contacts.slideDown(duration);
+				},duration - 150)
+				flag = false;
+			} else {
+				link.removeClass(activeLink);
+				contacts.slideUp(duration,function(){
+					setTimeout(function(){
+						contacts.removeAttr('style');
+					});
+				});
+				setTimeout(function(){
+					list.slideUp(duration,function(){
+						setTimeout(function(){
+							list.removeAttr('style');
+						});
+					});
+				},duration - 150)
+				flag = true;
+			}
+		});
 })();
